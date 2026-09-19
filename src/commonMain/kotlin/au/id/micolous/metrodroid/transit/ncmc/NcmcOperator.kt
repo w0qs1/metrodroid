1df543b9e7f85e52ae17a989700a1fa7838a2415
Sandeep Kumar <56790764+w0qs1@users.noreply.github.com>
2026-09-19 09:26:23 +0000

    Refactored Operator db

--- src/commonMain/kotlin/au/id/micolous/metrodroid/transit/ncmc/NcmcOperator.kt
+++ src/commonMain/kotlin/au/id/micolous/metrodroid/transit/ncmc/NcmcOperator.kt
@@ -3,30 +3,43 @@ package au.id.micolous.metrodroid.transit.ncmc
 import au.id.micolous.metrodroid.transit.Trip
 
 enum class NcmcOperator(
+    val acquirerId: Int,
+    val operatorId: Int,
     val displayName: String,
     val mode: Trip.Mode
 ) {
     CHENNAI_METRO(
+        acquirerId = 0x0B,
+        operatorId = 0x177D,
         displayName = "Chennai Metro",
         mode = Trip.Mode.METRO
     ),
 
-    HYDERABAD_METRO(
-        displayName = "Hyderabad Metro",
-        mode = Trip.Mode.METRO
-    ),
 
     BENGALURU_METRO(
+        acquirerId = 0x02,
+        operatorId = 0x0001,
         displayName = "Bengaluru Metro",
         mode = Trip.Mode.METRO
     ),
 
     MTC_CHENNAI(
+        acquirerId = 0x01,
+        operatorId = 0x0001,
         displayName = "MTC Chennai",
         mode = Trip.Mode.BUS
     ),
 
+    HYDERABAD_METRO(
+        acquirerId = 0x04,
+        operatorId = 0x3630,
+        displayName = "Hyderabad Metro",
+        mode = Trip.Mode.METRO
+    ),
+    
     UNKNOWN(
+        acquirerId = -1,
+        operatorId = -1,
         displayName = "Unknown",
         mode = Trip.Mode.OTHER
     );
@@ -36,25 +49,10 @@ enum class NcmcOperator(
             acquirerId: Int,
             operatorId: Int
         ): NcmcOperator {
-            return when {
-                acquirerId == 0x0B &&
-                    operatorId == 0x177D ->
-                    CHENNAI_METRO
-
-                acquirerId == 0x04 &&
-                    operatorId == 0x3630 ->
-                    HYDERABAD_METRO
-
-                acquirerId == 0x02 &&
-                    operatorId == 0x0001 ->
-                    BENGALURU_METRO
-
-                acquirerId == 0x01 &&
-                    operatorId == 0x0001 ->
-                    MTC_CHENNAI
-
-                else -> UNKNOWN
-            }
+            return entries.firstOrNull {
+                it.acquirerId == acquirerId &&
+                    it.operatorId == operatorId
+            } ?: UNKNOWN
         }
     }
 }
